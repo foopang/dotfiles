0de81884b10aac481adda2a0e48fc1e1f02e39c3
Foo Pang <foo.pang@gmail.com>
2018-03-15 07:12:53 +0800

    Update profiles.clj for CIDER

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
+{:user {:plugins [[refactor-nrepl "2.4.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.16.0"]
                   [lein-ancient "0.6.8"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [com.cemerick/piggieback "0.2.1"]]
